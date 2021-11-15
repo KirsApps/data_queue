3f8752dc1dbc7b368b4e625316073b8126caa066
Arthur Kirsanov <kirsappsinc@gmail.com>
2021-11-15 21:02:30 +0300

    QueueWorker documentation

--- lib/src/queue_worker.dart
+++ lib/src/queue_worker.dart
@@ -3,24 +3,38 @@ import 'package:meta/meta.dart';
 import 'commands.dart';
 import 'data_queue.dart';
 
+/// The class handles [dataQueue] with built-in commands.
 class QueueWorker<TEvent> {
+  /// Creates [QueueWorker] handles [dataQueue] with built-in commands.
   QueueWorker(this.dataQueue);
+
+  /// Data queue
   @protected
   final DataQueue<TEvent> dataQueue;
 
+  /// Returns all events from [dataQueue].
   Future<List<TEvent>> get all => dataQueue.execute<List<TEvent>>(AllCommand());
 
+  /// Returns an event from [dataQueue] without consuming it.
   Future<TEvent> get clone => dataQueue.execute<TEvent>(CloneCommand());
 
+  /// Returns events count in [dataQueue].
   Future<int> get enumerate => dataQueue.execute(EnumerateCommand());
 
+  /// Returns next event in [dataQueue].
   Future<TEvent> get next => dataQueue.execute<TEvent>(NextCommand());
 
+  /// Skips next [count] events in [dataQueue]
+  ///
+  /// The [count] must be non-negative.
   Future<int> skip(int count) {
     RangeError.checkNotNegative(count, 'count');
     return dataQueue.execute<int>(SkipCommand(count));
   }
 
+  /// Returns list with next [count] events from [dataQueue]
+  ///
+  /// The [count] must be non-negative.
   Future<List<TEvent>> take(int count) {
     RangeError.checkNotNegative(count, 'count');
     return dataQueue.execute<List<TEvent>>(TakeCommand(count));
